e03a0d8f8bfff7543590758c160dcca2e2745a09
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2022-06-22 15:38:21 -0500

    Update for functions examples with duplicates function

--- basics/ex_03_functions_for/main.go
+++ basics/ex_03_functions_for/main.go
@@ -9,29 +9,29 @@ import (
 )
 
 func main() {
-	var names = make([]string, 6)
-	fill_names(names)
+	fmt.Println("\n********** Starting processing of names **********")
 
-	fmt.Println("********** Starting processing **********")
+	var names []string = []string{"Santi", "Moni", "Elkin", "Yesid", "David", "Esteban"}
 	fmt.Println("NAMES: ", names)
+
+	// Please look at the 2 different "for" approaches on each "find_name_x" function
 	find_name_1("Yesid", names)
 	find_name_2("Yesid", names)
-}
 
-func fill_names(names []string) {
-	names[0] = "Santi"
-	names[1] = "Moni"
-	names[2] = "Elkin"
-	names[3] = "Yesid"
-	names[4] = "David"
-	names[5] = "Esteban"
+	fmt.Println("\n********** Starting processing of numbers **********")
+	var numbers []int = []int{1, 5, 3, 6, 7, 3, 4, 6}
+	fmt.Println("NUMBERS: ", numbers)
+
+	// This functions illustrates double "for" solutions with elements and indexes used
+	find_duplicates_1(numbers)
+	find_duplicates_2(numbers)
 }
 
 func find_name_1(name string, names []string) {
-	fmt.Println("\n----- Finding name only ----")
+	fmt.Println("\n----- Finding only the name ----")
 	for i := 0; i < len(names); i++ {
 		if names[i] == name {
-			fmt.Println("Name '", name, "' was found!")
+			fmt.Printf("Name: %q , was found!\n", name)
 			break
 		}
 	}
@@ -41,8 +41,35 @@ func find_name_2(name string, names []string) {
 	fmt.Println("\n----- Finding name and its index ----")
 	for index, item := range names {
 		if item == name {
-			fmt.Println("Name '", name, "' was found at index", index, "!")
+			fmt.Printf("Name: %q , was found at index: %d\n", name, index)
 			break
 		}
 	}
+}
+
+func find_duplicates_1(numbers []int) {
+	fmt.Println("\n----- Finding duplicates of array ----")
+	var duplicates []int
+	for i, element_1 := range numbers {
+		for j, element_2 := range numbers {
+			if element_1 == element_2 && i < j {
+				duplicates = append(duplicates, element_2)
+			}
+		}
+	}
+	fmt.Println("Duplicates are: ", duplicates)
+}
+
+func find_duplicates_2(numbers []int) {
+	fmt.Println("\n----- Finding duplicates of array ----")
+	var duplicates []int
+	for i, element_1 := range numbers {
+		for j := i + 1; j < len(numbers); j++ {
+			element_2 := numbers[j]
+			if element_1 == element_2 {
+				duplicates = append(duplicates, element_2)
+			}
+		}
+	}
+	fmt.Println("Duplicates are: ", duplicates)
 }
