acbb482948d621ab4cc79d0e977d879ca8e9e641
Santiago Garcia Arango <santiago.garcia1999@hotmail.com>
2022-07-05 09:34:24 -0500

    Update simple-crud-api with commands to create

---
+++ projects/simple-crud-api/commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Initialize go project
+go mod init github.com/san99tiago/go-gopher-go/tree/main/projects/simple-crud-api
+
+# Add Gorilla Mux dependency
+go get -u github.com/gorilla/mux
